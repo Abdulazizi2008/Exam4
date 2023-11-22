d491e7aced2498f375315c450eabd219f9210414
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2023-11-22 17:30:17 +0500

    page changet

--- js/main.js
+++ js/main.js
@@ -33,3 +33,8 @@ const menuBtn = document.querySelector(".menu");
 menuBtn.addEventListener("click", () => {
   header.classList.toggle("open");
 });
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
